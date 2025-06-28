aac533c446ce820713be91991a15926568398ee5
MOHANRAJ VELMANI <164868046+Mohanraj02V@users.noreply.github.com>
2025-06-28 14:54:43 +0530

    Update urls.py

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import PredictView, PredictionHistoryView
-
+# Temporary view for testing
+def home(request):
+    return HttpResponse("Churn Prediction API is running!")
 urlpatterns = [
+    path('', home),
     path('predict/', PredictView.as_view(), name='predict'),
     path('history/', PredictionHistoryView.as_view(), name='prediction-history'),
 ]
