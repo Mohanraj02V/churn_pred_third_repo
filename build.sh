8113634493822119adde9ba24b8d2b3e5604dba2
MOHANRAJ VELMANI <164868046+Mohanraj02V@users.noreply.github.com>
2025-06-28 08:02:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-# Add this before installing requirements
-pip install --upgrade pip setuptools wheel
-pip install cython==3.0.0
-
-# Install requirements
 pip install -r requirements.txt
-
-# Rebuild scikit-learn
-pip install --no-build-isolation --force-reinstall scikit-learn
-
-# Standard Django commands
 python manage.py collectstatic --noinput
 python manage.py migrate
