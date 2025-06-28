9c8c0f0869b5ea3bb9a6afb3d80c33c4f6948c76
MOHANRAJ VELMANI <164868046+Mohanraj02V@users.noreply.github.com>
2025-06-28 11:29:19 +0530

    Update settings.py

--- churn_predictor/settings.py
+++ churn_predictor/settings.py
@@ -89,18 +89,13 @@ DATABASES = {
 }
 '''
 import dj_database_url
+import os
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 '''
 if os.environ.get('DATABASE_URL'):
     DATABASES = {
